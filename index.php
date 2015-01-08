c2c869d8517df3b01f47e9dfcf9af37f182b27c0
Eric Martinez <martinez_eric82@yahoo.com>
2015-01-07 18:13:57 -0800

    made two of my exmples

---
+++ index.php
@@ -0,0 +1,41 @@
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+    </head>
+    <body>
+        <?php
+        class Car {
+            public $firstName = "default name";
+            public $year = "default name";
+            public $price = "0";
+        
+            function getName(){
+                return"{$this->firstName}" .
+                "{$this->year}";        
+            }
+        }
+        $Car1 = new Car();
+        $Car1->firstName = "Honda";
+        $Car1->year = "2005";
+        
+        print"The car's name is {$Car1->getName()}";
+        //this will print out to the screen.
+        
+        class iphone{
+            public $firstName = "default name";
+            public $type = "default";
+            public $price = "0";
+            
+            function getName(){
+                return"{$this->firstName}" .
+                "{$this->type}";        
+            } 
+        }
+        $iphone1 = new iphone();
+        $iphone1->firstName = "iphone";
+        $iphone1->type = "6 plus";
+        print"The phone is a {$iphone1->getName()}";
+        ?>
+    </body>
+</html>
