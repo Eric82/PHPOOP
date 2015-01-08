02f9e9e4e90c55fc2f0e2185c76a18a7a2ae738a
Eric Martinez <martinez_eric82@yahoo.com>
2015-01-07 18:49:56 -0800

    finished the example in lecture 2

---
+++ lesson 2.php
@@ -0,0 +1,21 @@
+<?php
+
+class Dog{
+    public $firstName;
+    public $lastName;
+    public $breed;
+
+    function __construct($firstName,$lastName,$breed){
+        $this->firstName = $firstName;
+        $this->lastName = $lastName;
+        $this->breed = $breed;
+    }
+    function getName(){
+        return"{$this->$firstName}" .
+        "{$this->lastName}";   
+    } 
+}
+    $dog1 = new Dog("BooBoo", "Iwao", "EnglishBulldog");
+    print "Dog 1: {$dog1->getName()}";
+
+?>
